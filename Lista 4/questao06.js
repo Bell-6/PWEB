32b796252ee432a18ad1c139b7b6113f9e661eff
M. Isabella <maria.bella0619@gmail.com>
2025-09-25 08:31:39 -0300

    Create questao06.js

---
+++ Lista 4/questao06.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+const nomecorret = "Maria";
+const senhacorret = "1234";
+
+let nome = prompt("Digite seu nome: ");
+let senha = prompt("Digite sua senha: ");
+
+if (nome == nome corret && senha == senhacorret){
+console.log("Acesso permitido"); 
+  
+}
+else {
+console.log("Acesso negado");
+}
+
