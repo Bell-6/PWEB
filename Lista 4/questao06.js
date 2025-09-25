b636ef96e66838337384b172dac7125a975cdc52
M. Isabella <maria.bella0619@gmail.com>
2025-09-25 08:31:52 -0300

    Update questao06.js

--- Lista 4/questao06.js
+++ Lista 4/questao06.js
@@ -6,7 +6,7 @@ const senhacorret = "1234";
 let nome = prompt("Digite seu nome: ");
 let senha = prompt("Digite sua senha: ");
 
-if (nome == nome corret && senha == senhacorret){
+if (nome == nomecorret && senha == senhacorret){
 console.log("Acesso permitido"); 
   
 }
